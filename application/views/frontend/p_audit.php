d388cf14c32ba4f4167b207a746caee09d7d8606
PanupongASsystem <PanupongI@assystem.co.th>
2024-04-10 12:17:51 +0700

    ว่าง

--- application/views/frontend/p_audit.php
+++ application/views/frontend/p_audit.php
@@ -1,36 +1,83 @@
 <div class="bg-pages ">
-    <div class="container-pages-detail">
-        <div class="page-center">
-            <div class="head-pages">
-                <span class="font-pages-head-long">หน่วยตรวจสอบภายใน</span>
-            </div>
+    <div class="row pad-path">
+        <div class="path1-1">
+            <span class="font-path-1 underline"><a href="<?php echo site_url('Home'); ?>">หน้าแรก</a></span>
         </div>
-        <div class="row">
-            <div class="path1-1">
-                <span class="font-path-1 underline"><a href="<?php echo site_url('Home'); ?>">หน้าแรก</a></span>
-            </div>
-            <div class="path2-4">
-                <span class="font-path-2 underline"><a href="#">โครงสร้างบุคลากร</a></span>
-            </div>
+        <div class="path2-3">
+            <span class="font-path-2 underline"><a href="#">โครงสร้างบุคลากร</a></span>
         </div>
-        <div class="bg-pages-in">
-            <div class="scrollable-container">
-                <div class="page-center">
-                    <?php foreach ($rsOne as $rs) { ?>
+    </div>
+    <div class="page-center">
+        <div class="head-pages-two">
+            <span class="font-pages-head">กองประปา</span>
+        </div>
+    </div>
+    <div class="bg-pages-p">
+        <div class="scrollable-container">  
+            <div class="page-center">
+                <?php foreach ($query_one as $rs) { ?>
+                    <!-- --/ -->
+                    <?php if (!empty($rs->p_audit_rank)) : ?>
+                        <!-- === -->
                         <div class="bg-personnel-s">
                             <div class="rounded-image-s">
                                 <img src="<?= base_url('docs/img/' . $rs->p_audit_img); ?>" width="100%" height="100%">
                             </div>
                         </div>
                         <div class="mt-3 center-center">
-                            <span class="font-p-name"><?= $rs->p_audit_name; ?></span>
+                            <!-- == -->
+                            <span class="font-p-name">
+                                <?php echo !empty($rs->p_audit_name) ? $rs->p_audit_name : 'ว่าง'; ?>
+                            </span>
+                            <!-- == -->
                             <span class="font-p-detail "><?= $rs->p_audit_rank; ?></span>
                             <?php if (!empty($rs->p_audit_phone)) : ?>
                                 <span class="font-p-detail">เบอร์ <?= $rs->p_audit_phone; ?></span>
                             <?php endif; ?>
                         </div>
-                    <?php } ?>
-                </div>
+                    <?php endif; ?>
+                <?php } ?>
+            </div>
+            <div class="row " style="margin-top: 25px;">
+                <?php foreach ($query_under_one as $rs) : ?>
+                    <div class="col-4 col-md-4 mb-3 center-center">
+                        <!-- == -->
+                        <?php if (!empty($rs->p_audit_rank)) : ?>
+                            <!-- == -->
+                            <div class="bg-personnel-s">
+                                <div class="rounded-image-s">
+                                    <img src="<?= base_url('docs/img/' . $rs->p_audit_img); ?>" width="100%" height="100%">
+                                </div>
+                            </div>
+                            <!-- == -->
+                            <span class="font-p-name">
+                                <?php echo !empty($rs->p_audit_name) ? $rs->p_audit_name : 'ว่าง'; ?>
+                            </span>
+                            <!-- == -->
+                            <span class="font-p-detail "><?= $rs->p_audit_rank; ?></span>
+                            <?php if (!empty($rs->p_audit_phone)) : ?>
+                                <span class="font-p-detail">เบอร์ <?= $rs->p_audit_phone; ?></span>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+            <!-- <?php foreach ($rsOne as $rs) { ?>
+                    <div class="bg-personnel-s">
+                        <div class="rounded-image-s">
+                            <img src="<?= base_url('docs/img/' . $rs->p_audit_img); ?>" width="100%" height="100%">
+                        </div>
+                    </div>
+                    <div class="mt-3 center-center">
+                        <span class="font-p-name"><?= $rs->p_audit_name; ?></span>
+                        <span class="font-p-detail "><?= $rs->p_audit_rank; ?></span>
+                        <?php if (!empty($rs->p_audit_phone)) : ?>
+                            <span class="font-p-detail">เบอร์ <?= $rs->p_audit_phone; ?></span>
+                        <?php endif; ?>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="scrollable-container-gi">
                 <div class="row " style="margin-top: 25px;">
                     <?php foreach ($rsrow1 as $rs) : ?>
                         <div class="col-4 col-md-4 mb-3 center-center">
@@ -115,9 +162,11 @@
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
-                </div>
-                <br>
-            </div>
+                </div> -->
+        </div>
+        <div class="margin-top-delete-topic d-flex justify-content-end mt-2">
+            <a href="<?php echo site_url('Home'); ?>"><img src="<?php echo base_url("docs/k.btn-back.png"); ?>"></a>
         </div>
     </div>
+</div>
 </div>
--- application/views/frontend/p_dsab.php
+++ application/views/frontend/p_dsab.php
@@ -17,22 +17,22 @@
             <div class="page-center">
                 <?php foreach ($query_one as $rs) { ?>
                     <!-- --/ -->
-                    <?php if (!empty($rs->P_dsab_rank)) : ?>
+                    <?php if (!empty($rs->p_dsab_rank)) : ?>
                         <!-- === -->
                         <div class="bg-personnel-s">
                             <div class="rounded-image-s">
-                                <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                                <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                             </div>
                         </div>
                         <div class="mt-3 center-center">
                             <!-- == -->
                             <span class="font-p-name">
-                                <?php echo !empty($rs->P_dsab_name) ? $rs->P_dsab_name : 'ว่าง'; ?>
+                                <?php echo !empty($rs->p_dsab_name) ? $rs->p_dsab_name : 'ว่าง'; ?>
                             </span>
                             <!-- == -->
-                            <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                            <?php if (!empty($rs->P_dsab_phone)) : ?>
-                                <span class="font-p-detail">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                            <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                            <?php if (!empty($rs->p_dsab_phone)) : ?>
+                                <span class="font-p-detail">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                             <?php endif; ?>
                         </div>
                     <?php endif; ?>
@@ -42,21 +42,21 @@
                 <?php foreach ($query_under_one as $rs) : ?>
                     <div class="col-4 col-md-4 mb-3 center-center">
                         <!-- == -->
-                        <?php if (!empty($rs->P_dsab_rank)) : ?>
+                        <?php if (!empty($rs->p_dsab_rank)) : ?>
                             <!-- == -->
                             <div class="bg-personnel-s">
                                 <div class="rounded-image-s">
-                                    <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                                    <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                                 </div>
                             </div>
                             <!-- == -->
                             <span class="font-p-name">
-                                <?php echo !empty($rs->P_dsab_name) ? $rs->P_dsab_name : 'ว่าง'; ?>
+                                <?php echo !empty($rs->p_dsab_name) ? $rs->p_dsab_name : 'ว่าง'; ?>
                             </span>
                             <!-- == -->
-                            <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                            <?php if (!empty($rs->P_dsab_phone)) : ?>
-                                <span class="font-p-detail">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                            <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                            <?php if (!empty($rs->p_dsab_phone)) : ?>
+                                <span class="font-p-detail">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                             <?php endif; ?>
                         <?php endif; ?>
                     </div>
@@ -65,14 +65,14 @@
             <!-- <?php foreach ($rsOne as $rs) { ?>
                     <div class="bg-personnel-s">
                         <div class="rounded-image-s">
-                            <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                            <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                         </div>
                     </div>
                     <div class="mt-3 center-center">
-                        <span class="font-p-name"><?= $rs->P_dsab_name; ?></span>
-                        <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                        <?php if (!empty($rs->P_dsab_phone)) : ?>
-                            <span class="font-p-detail">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                        <span class="font-p-name"><?= $rs->p_dsab_name; ?></span>
+                        <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                        <?php if (!empty($rs->p_dsab_phone)) : ?>
+                            <span class="font-p-detail">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                         <?php endif; ?>
                     </div>
                 <?php } ?>
@@ -83,13 +83,13 @@
                         <div class="col-4 col-md-4 mb-3 center-center">
                             <div class="bg-personnel-s">
                                 <div class="rounded-image-s">
-                                    <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                                    <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                                 </div>
                             </div>
-                            <span class="font-p-name"><?= $rs->P_dsab_name; ?></span>
-                            <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                            <?php if (!empty($rs->P_dsab_phone)) : ?>
-                                <span class="font-p-detail">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                            <span class="font-p-name"><?= $rs->p_dsab_name; ?></span>
+                            <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                            <?php if (!empty($rs->p_dsab_phone)) : ?>
+                                <span class="font-p-detail">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
@@ -100,13 +100,13 @@
                         <div class="col-4 col-md-4 mb-3 center-center">
                             <div class="bg-personnel-s">
                                 <div class="rounded-image-s">
-                                    <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                                    <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                                 </div>
                             </div>
-                            <span class="font-p-name "><?= $rs->P_dsab_name; ?></span>
-                            <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                            <?php if (!empty($rs->P_dsab_phone)) : ?>
-                                <span class="font-p-detail ">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                            <span class="font-p-name "><?= $rs->p_dsab_name; ?></span>
+                            <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                            <?php if (!empty($rs->p_dsab_phone)) : ?>
+                                <span class="font-p-detail ">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                             <?php endif; ?>
                             <br>
                         </div>
@@ -118,13 +118,13 @@
                         <div class="col-4 col-md-4 mb-3 center-center">
                             <div class="bg-personnel-s">
                                 <div class="rounded-image-s">
-                                    <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                                    <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                                 </div>
                             </div>
-                            <span class="font-p-name "><?= $rs->P_dsab_name; ?></span>
-                            <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                            <?php if (!empty($rs->P_dsab_phone)) : ?>
-                                <span class="font-p-detail ">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                            <span class="font-p-name "><?= $rs->p_dsab_name; ?></span>
+                            <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                            <?php if (!empty($rs->p_dsab_phone)) : ?>
+                                <span class="font-p-detail ">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
@@ -135,13 +135,13 @@
                         <div class="col-4 col-md-4 mb-3 center-center">
                             <div class="bg-personnel-s">
                                 <div class="rounded-image-s">
-                                    <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                                    <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                                 </div>
                             </div>
-                            <span class="font-p-name "><?= $rs->P_dsab_name; ?></span>
-                            <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                            <?php if (!empty($rs->P_dsab_phone)) : ?>
-                                <span class="font-p-detail ">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                            <span class="font-p-name "><?= $rs->p_dsab_name; ?></span>
+                            <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                            <?php if (!empty($rs->p_dsab_phone)) : ?>
+                                <span class="font-p-detail ">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
@@ -152,13 +152,13 @@
                         <div class="col-4 col-md-4 mb-3 center-center">
                             <div class="bg-personnel-s">
                                 <div class="rounded-image-s">
-                                    <img src="<?= base_url('docs/img/' . $rs->P_dsab_img); ?>" width="100%" height="100%">
+                                    <img src="<?= base_url('docs/img/' . $rs->p_dsab_img); ?>" width="100%" height="100%">
                                 </div>
                             </div>
-                            <span class="font-p-name "><?= $rs->P_dsab_name; ?></span>
-                            <span class="font-p-detail "><?= $rs->P_dsab_rank; ?></span>
-                            <?php if (!empty($rs->P_dsab_phone)) : ?>
-                                <span class="font-p-detail ">เบอร์ <?= $rs->P_dsab_phone; ?></span>
+                            <span class="font-p-name "><?= $rs->p_dsab_name; ?></span>
+                            <span class="font-p-detail "><?= $rs->p_dsab_rank; ?></span>
+                            <?php if (!empty($rs->p_dsab_phone)) : ?>
+                                <span class="font-p-detail ">เบอร์ <?= $rs->p_dsab_phone; ?></span>
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
