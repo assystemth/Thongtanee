5b39addb7a99542a4eb306d97fb50b94bceaeb66
assystemth <dev@assystem.co.th>
2024-04-26 12:05:09 +0700

    search

--- application/views/frontend_templat/navbar.php
+++ application/views/frontend_templat/navbar.php
@@ -27,6 +27,102 @@
             display: none;
         }
     }
+
+    .search {
+        width: 250px;
+        position: absolute;
+        top: -15%;
+        left: 77%;
+    }
+
+    .gsc-search-button-v2 {
+        /* background-color: #007bff; */
+        /* ปรับสีพื้นหลังของปุ่มค้นหาเป็นสีฟ้า */
+        color: #ffffff;
+        /* ปรับสีของข้อความในปุ่มเป็นสีขาว */
+        padding: 5px 10px;
+        /* ปรับการเรียงขนาดของปุ่ม */
+        border-radius: 5px;
+        /* ปรับรูปร่างของปุ่มเป็นรูปแบบวงกลม */
+        border: none;
+        /* ลบเส้นขอบของปุ่ม */
+    }
+
+    .gsc-search-button-v2 svg {
+        fill: #523003;
+        /* ปรับสีของไอคอนเป็นสีขาว */
+        width: 20px;
+        /* ปรับขนาดของไอคอนเป็น 15px */
+        height: 20px;
+        /* ปรับขนาดของไอคอนเป็น 15px */
+    }
+
+    .gsc-control-cse {
+        background-color: transparent;
+        border: none;
+    }
+
+    .gsc-search-button-v2:hover {
+        /* background-color: #0056b3; */
+        /* color: ; */
+        /* ปรับสีของปุ่มเมื่อเม้าส์ hover */
+    }
+
+    /* ซ่อนข้อความ "เพิ่มประสิทธิภาพโดย Google" */
+    .gsc-input-box .gsc-input {
+        color: transparent;
+    }
+
+    /* เพิ่ม placeholder แทนข้อความ "เพิ่มประสิทธิภาพโดย Google" */
+    .gsc-input-box::before {
+        content: 'ค้นหา';
+        color: #000;
+        /* เปลี่ยนสีข้อความ placeholder ตามต้องการ */
+        position: absolute;
+        top: 12px;
+        /* ปรับตำแหน่งตามต้องการ */
+        left: 10px;
+        /* ปรับตำแหน่งตามต้องการ */
+        z-index: -1;
+        /* สร้างข้อความ placeholder ให้อยู่ต่ำกว่า input */
+    }
+
+
+    .gsc-control {
+        font-family: arial, sans-serif;
+        background-color: lightblue !important;
+        width: 500px;
+        border-radius: 3rem;
+        padding: 7px 20px !important;
+    }
+
+    .gsc-input {
+        padding: 0px !important;
+    }
+
+    .gsc-input-box {
+        border: 1px solid #dfe1e5;
+        background: #fff;
+        border-radius: 2rem;
+        padding: 1px 10px;
+        position: relative;
+    }
+
+    #gsc-i-id1 {
+        color: #000 !important;
+        line-height: 1.2 !important;
+        background: none !important;
+        font-size: 1rem !important;
+    }
+
+    .gsc-search-button-v2 {
+        padding: 0.5rem !important;
+        cursor: pointer;
+        border-radius: 50%;
+        position: absolute;
+        margin-left: -33px;
+        margin-top: -18px;
+    }
 </style>
 <!-- <nav class="navbar navbar2 navbar-expand-lg navbar-dark navbar-center sticky-top" id="navbar2">
     <div class="container">
@@ -65,6 +161,38 @@
             }
         }
     }
+
+     // เปลี่ยนคำเป็นคำว่า ค้นหา
+     window.onload = function() {
+        var placeHolderText = "ค้นหา";
+        var searchBox = document.querySelector("#gsc-i-id1");
+        var searchButton = document.querySelector(".gsc-search-button-v2 svg title");
+        searchBox.placeholder = placeHolderText;
+        searchBox.title = placeHolderText;
+        searchButton.innerHTHL = placeHolderText;
+    }
+
+    // ค้นหาซ่อน / แสดงผล
+    function toggleSearch() {
+        var searchContainer = document.getElementById('searchContainer');
+        var searchImage = document.getElementById('searchImage');
+
+        if (searchContainer.style.display === 'none') {
+            searchContainer.style.display = 'block'; // แสดง div
+            searchImage.style.display = 'none'; // ซ่อนรูป
+        } else {
+            searchContainer.style.display = 'none'; // ซ่อน div
+            searchImage.style.display = 'block'; // แสดงรูป
+        }
+    }
+
+    function changeImage(imageUrl) {
+        document.getElementById('searchImage').src = imageUrl;
+    }
+
+    function restoreImage(imageUrl) {
+        document.getElementById('searchImage').src = imageUrl;
+    }
 </script>
 
 
@@ -79,6 +207,14 @@
     <a href="<?php echo site_url('Home/login'); ?>" onmouseover="changeImage(`<?php echo base_url('docs/k.item-nav-top3v3-hover.png'); ?>`)" onmouseout="restoreImage(`<?php echo base_url('docs/k.item-nav-top3v3.png'); ?>`)">
         <img src="<?php echo base_url("docs/k.item-nav-top3v3.png"); ?>" alt="" style="position: absolute; top: 20%; left: 88%;">
     </a>
+    <div class="search">
+        <a href="#" id="searchShow" onclick="toggleSearch()">
+            <img id="searchImage" src="<?php echo base_url("docs/search.png"); ?>" style="position: absolute; top: 10%; left: 84%;" onmouseover="changeImage('<?php echo base_url('docs/search-hover.png'); ?>')" onmouseout="restoreImage('<?php echo base_url('docs/search.png'); ?>')">
+        </a>
+        <div id="searchContainer" style="display: none;">
+            <div class="gcse-search"></div>
+        </div>
+    </div>
 </div>
 
 
